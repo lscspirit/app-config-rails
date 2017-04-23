4a4e26e14563e2d18ed2c4d71f651f4339ae9137
lscspirit <lscspirit@hotmail.com>
2017-04-23 16:10:28 +0800

    RSpec test cases for ConfigEntry#<=>

--- spec/lib/config_entry_spec.rb
+++ spec/lib/config_entry_spec.rb
@@ -1,6 +1,24 @@
 require 'spec_helper'
 
 RSpec.describe AppConfigLoader::ConfigEntry do
+  def build_full_key(key_size, env, domain, use_domain = false)
+    parts = [env]
+    parts << domain if use_domain
+    key_size.times { parts << Faker::Lorem.characters(10) }
+
+    parts.join('.')
+  end
+
+  def compare_key_components(main_components, other_components)
+    comp = main_components.count <=> other_components.count
+    return comp if comp != 0
+
+    main_components.each_index do |idx|
+      comp = main_components[idx] <=> other_components[idx]
+      return comp if comp != 0
+    end
+  end
+
   describe '#applicable?' do
     let(:result) { entry.applicable?(*params) }
 
@@ -125,6 +143,71 @@ RSpec.describe AppConfigLoader::ConfigEntry do
             expect(result).to be_truthy
           end
         end
+      end
+    end
+  end
+
+  describe '#<=>' do
+    subject { main <=> other }
+
+    let(:use_domain) { [true, false].sample }
+    let(:main)  { described_class.new main_key, Faker::Lorem.characters(10), use_domain }
+    let(:other) { described_class.new other_key, Faker::Lorem.characters(10), use_domain }
+
+    let(:key_size) { (1..6).to_a.sample(2) }
+    let(:envs) { %w(test development production *).sample(2) }
+    let(:domains) { [Faker::Lorem.characters(5), Faker::Lorem.characters(5)] }
+
+    context 'with entries of different key size' do
+      let(:main_key) { build_full_key key_size[0], envs[0], domains[0], use_domain }
+      let(:other_key) { build_full_key key_size[1], envs[1], domains[1], use_domain }
+
+      it 'returns the same comparison as the key size comparison' do
+        expect(subject).to eq(compare_key_components(main.key_components, other.key_components))
+      end
+    end
+
+    context 'with entries same key size but different key' do
+      let(:main_key) { build_full_key key_size[0], envs[0], domains[0], use_domain }
+      let(:other_key) { build_full_key key_size[0], envs[1], domains[1], use_domain }
+
+      it 'returns the same comparison as the key component comparison' do
+        expect(subject).to eq(compare_key_components(main.key_components, other.key_components))
+      end
+    end
+
+    context 'with entries same key but different specificity' do
+      let(:envs) { ['*', Faker::Lorem.characters(5)].shuffle }  # this guarantee one of the env is wildcard
+      let(:main_key) { build_full_key key_size[0], envs[0], domains[0], use_domain }
+      let(:other_key) { main_key.gsub(/^[^.]+/, envs[1]) }
+
+      it 'returns the same comparison between the specificity of the two entries' do
+        expect(subject).to eq(main.specificity <=> other.specificity)
+      end
+    end
+
+    context 'with entries same key and specificity but different env' do
+      let(:envs) { [Faker::Lorem.characters(5), Faker::Lorem.characters(5)] }
+      let(:main_key) { build_full_key key_size[0], envs[0], domains[0], use_domain }
+      let(:other_key) { main_key.gsub(/^[^.]+/, envs[1]) }
+
+      it 'returns the lexcial comparison of the two envs' do
+        expect(subject).to eq(envs[0] <=> envs[1])
+      end
+    end
+
+    context 'with entries same key, specificity and env but different domain' do
+      let(:use_domain) { true }
+      let(:domains) { [Faker::Lorem.characters(5), Faker::Lorem.characters(5)] }
+      let(:main_key) { build_full_key key_size[0], envs[0], domains[0], use_domain }
+      let(:other_key) do
+        parts = main_key.split('.')
+        parts[1] = domains[1]
+        parts.join('.')
+      end
+
+      it 'returns the lexical comparison of the two envs' do
+        expect(subject).to eq(domains[0] <=> domains[1])
       end
     end
   end
